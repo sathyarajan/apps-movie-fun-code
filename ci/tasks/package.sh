b487f69a4400fef67f77bb4ed8bc8e457b30045b
Neethirajan <263649@AMB00682.local>
2017-07-21 12:50:32 +0530

    war file

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -3,18 +3,18 @@
 set -e +x
 
 pushd apps-movie-fun-code-source
-  echo "Packaging JAR"
-  ./mvnw clean package -DskipTests
+  echo "Packaging WAR"
+  mvn clean package -DskipTests
 popd
 
-jar_count=`find apps-movie-fun-code-source/target -type f -name *.jar | wc -l`
+jar_count=`find apps-movie-fun-code-source/target -type f -name *.war | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find apps-movie-fun-code-source/target -type f -name *.jar -exec cp "{}" package-output/movie-app.jar \;
+find apps-movie-fun-code-source/target -type f -name *.war -exec cp "{}" package-output/movie-app.war \;
 
 echo "Done packaging"
 exit 0
